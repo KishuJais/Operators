08060dc28bd739d55765d7c702c868a5d8cb2ac9
KishuJais <kishujaiswal96@gmail.com>
2022-12-14 22:50:54 +0530

    loops

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,13 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i = 1; // this is called initialization
+		do {
+			System.out.println(i);
+			i++; //this is increment i++ = i+1
+		} while (i <= 10);  //This is called test condition
+			
+			}
+		}
